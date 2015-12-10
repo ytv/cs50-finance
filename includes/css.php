e66e96a53efe1b8b7693ab8f739a957978932ebe
ytv <sillying@gmail.com>
2015-12-10 18:38:41 -0500

    first commit

---
+++ includes/css.php
@@ -0,0 +1,62 @@
+<?php
+/*****
+*
+* css.php
+*
+* To add css styling to pages
+*
+* ytv
+*
+*
+**/?>
+
+<style>
+
+    ul.nav li {
+        display: inline-block;
+    }
+    
+    ul.settings
+    {
+        padding: 20px;
+        list-style-type: none;
+    }
+    
+    table{
+        width: 80%;
+        margin-left:10%;
+        margin-right:10%;
+        margin-top:20px;
+        margin-bottom:20px;   
+    }
+    
+    tr th{
+        text-align: center;
+        padding: 7px;
+    }
+    
+    tr{
+        height: 30px;
+    }
+    
+    tr:nth-child(even) {background-color: #f2f2f2}
+    
+    h4{
+        padding-top: 15px;
+    }
+    
+    form{
+        padding: 20px;
+    }
+    
+    p.success{
+        color: green;
+        padding-top: 20px;
+        padding-bottom:5px;
+    }
+    
+</style>
+
+        <link href="/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="/css/bootstrap-theme.min.css" rel="stylesheet"/>
+        <link href="/css/styles.css" rel="stylesheet"/>
